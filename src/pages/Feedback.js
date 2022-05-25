08cc29988a713cdc90fb2b53b4c0743e26333ee7
Igor Castro <ncastro.igor@gmail.com>
2022-05-25 19:44:35 -0400

    Requisitos 13, 14, 15, 16 e 18 finalizados

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -1,18 +1,78 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import '../App.css';
 import Header from '../redux/components/Header';
 
-export default class Feedback extends Component {
+class Feedback extends Component {
+  clickedPlayAgain = () => {
+    const { history } = this.props;
+    history.push('/');
+  }
+
+  clickedToRanking = () => {
+    const { history } = this.props;
+    history.push('/ranking');
+  }
+
   render() {
+    const { score, assertions } = this.props;
     return (
       <div>
         <Header />
+        {assertions < 3 ? (
+          <h3
+            data-testid="feedback-text"
+          >
+            Could be better...
+          </h3>
+        ) : (
+          <h3
+            data-testid="feedback-text"
+          >
+            Well Done!
+          </h3>
+        )}
+        <p>Placar Final:</p>
         <p
-          data-testid="feedback-text"
+          data-testid="feedback-total-score"
         >
-          FEEDBACK
+          { score }
         </p>
+        <p>Acertos:</p>
+        <p
+          data-testid="feedback-total-question"
+        >
+          { assertions }
+        </p>
+        <button
+          type="button"
+          data-testid="btn-play-again"
+          onClick={ this.clickedPlayAgain }
+        >
+          Play Again
+        </button>
+        <button
+          type="button"
+          data-testid="btn-ranking"
+          onClick={ this.clickedToRanking }
+        >
+          Ranking
+        </button>
       </div>
     );
   }
 }
+
+Feedback.propTypes = {
+  score: PropTypes.number.isRequired,
+  assertions: PropTypes.number.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  score: state.player.score,
+  assertions: state.player.assertions,
+});
+
+export default connect(mapStateToProps)(Feedback);
+
--- src/pages/Ranking.js
+++ src/pages/Ranking.js
@@ -2,10 +2,26 @@ import React, { Component } from 'react';
 import '../App.css';
 
 export default class Ranking extends Component {
+  clickedGoHome = () => {
+    const { history } = this.props;
+    history.push('/');
+  }
+
   render() {
     return (
       <div>
-        Ranking
+        <h2
+          data-testid="ranking-title"
+        >
+          Ranking
+        </h2>
+        <button
+          type="button"
+          data-testid="btn-go-home"
+          onClick={ this.clickedGoHome }
+        >
+          Go Home
+        </button>
       </div>
     );
   }
