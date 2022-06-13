ff69aafe93491697c2568741cf914471e1509766
Igor Castro <ncastro.igor@gmail.com>
2022-06-13 15:37:17 -0300

    Updates

---
+++ precache-manifest.959e4c3b3ff55a3332f3b6fb5dea68d2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "621f9624cf515e6a67dc9f0cebc6e1f4",
+    "url": "/project-trivia/index.html"
+  },
+  {
+    "revision": "8fef126f8e9ae6640cc4",
+    "url": "/project-trivia/static/css/main.68fbe302.chunk.css"
+  },
+  {
+    "revision": "f8d8714b0a9ae321c54d",
+    "url": "/project-trivia/static/js/2.aa77fb64.chunk.js"
+  },
+  {
+    "revision": "2d0dc3ea56ea29aa89323d664321ba6d",
+    "url": "/project-trivia/static/js/2.aa77fb64.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8fef126f8e9ae6640cc4",
+    "url": "/project-trivia/static/js/main.18fa2d7f.chunk.js"
+  },
+  {
+    "revision": "f125377fa2c39526d4b3",
+    "url": "/project-trivia/static/js/runtime-main.33b57c0b.js"
+  },
+  {
+    "revision": "466d153e266e6e813c5ef2c55aa5fc2c",
+    "url": "/project-trivia/static/media/trivia.466d153e.png"
+  }
+]);
